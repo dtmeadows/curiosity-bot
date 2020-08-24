0baedb9f0895e9aac54dc5a81feee7e42bf9046e
D Meadows <davidtmeadows@gmail.com>
2020-08-24 14:36:13 -0700

    all tests passing

--- commands/index.js
+++ commands/index.js
@@ -2,10 +2,12 @@ const Discord = require('discord.js');
 const ping = require('./ping.js');
 const help = require('./help.js');
 const pong = require('./pong.js');
+const curiosity = require('./curiosity.js');
 
 const commands = new Discord.Collection();
 
 commands.set(ping.name, ping);
+commands.set(curiosity.name, curiosity);
 commands.set(help.name, help);
 commands.set(pong.name, pong);
 
--- test/unit/commands/help.js
+++ test/unit/commands/help.js
@@ -6,7 +6,7 @@ const help = require('../../../commands/help.js');
 describe('help', () => {
   describe('with no args', () => {
     it('it returns commands list', async () => {
-      const expectedMessage = 'Available commands are: `ping, help`\nFor help with a specific command, send `$help command`';
+      const expectedMessage = 'Available commands are: `ping, curiosity, help`\nFor help with a specific command, send `$help command`';
 
       assert.equal(expectedMessage, await help.execute());
     });
@@ -19,6 +19,19 @@ describe('help', () => {
         + 'Description: `Ping!`';
 
       assert.equal(expectedMessage, await help.execute('ping'));
+    });
+
+    it('it provides help on a specific command', async () => {
+      const expectedMessage = 'Name: `curiosity`\n'
+        + 'Description: `Check deck export from MTG Arena against curiosity rules`\n'
+        + 'Usage: `curiosity [deck list]`\n'
+        + 'Examples:\n'
+        + '\t`curiosity\n'
+        + '2 Drowsing Tyrannodon (M21) 178\n'
+        + '8 Plains (IKO) 262\n'
+        + '2 Pridemalkin(M21) 196`';
+
+      assert.equal(expectedMessage, await help.execute('curiosity'));
     });
 
     it('it returns an error if no command is found', async () => {
